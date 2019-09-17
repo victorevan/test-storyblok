23abaed66ddbb64bc6b52d52e9f0fdc654050934
Victor Evangelista <victorevangelista@protonmail.com>
2019-09-17 01:03:31 -0700

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default class extends React.Component {
+	render() {
+		return (
+			<Layout>
+				<h1>Welcome to Next!</h1>
+			</Layout>
+		);
+	}
+}
