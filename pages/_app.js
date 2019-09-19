a0590a015f25e75e403e316f0e513ba8c621a329
Victor Evangelista <victorevangelista@protonmail.com>
2019-09-18 22:57:42 -0700

    remove global call

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,6 @@ import React from 'react';
 import App, { Container } from 'next/app';
 
 class MyApp extends App {
-	static async getInitialProps({ Component, ctx, router }) {
-		let pageProps = {};
-		if (Component.getInitialProps) {
-			pageProps = await Component.getInitialProps(ctx);
-		}
-		return {
-			pageProps
-		};
-	}
 	render() {
 		const { Component, pageProps } = this.props;
 		return <Component {...pageProps} />;
